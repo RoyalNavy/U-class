e9d3e376493403e76b4af2e356627cb433cfec4a
RoyalNavy <minggao92@gmail.com>
2014-05-27 21:58:54 +0000

    Initalize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UClass::Application.config.secret_key_base = 'b075689fb2308c411a7ecc7c19dcf383aab368e984190997d07554e4c1f0ca8bda55f64e98c7af141a967024bf7d3ccbc359cd913b17cec51c3a9728eac36ef0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UClass::Application.config.session_store :cookie_store, key: '_U-class_session'
