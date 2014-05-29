0d59a285629945bc85425cde3e8292514588296e
RoyalNavy <minggao92@gmail.com>
2014-05-29 21:46:48 +0000

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Ming Gao"
+    email    "minggao92@gmail.com"
+    password "Skyline@123"
+    password_confirmation "Skyline@123"
+  end
+end
